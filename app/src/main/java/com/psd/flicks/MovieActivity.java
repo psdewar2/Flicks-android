1d4dce33ee670700acf89af1e68f55f4b24ebc52
psdewar2 <psdewar2@live.com>
2016-07-18 00:04:30 -0400

    Movie objects are displayed in default list view

--- app/src/main/java/com/psd/flicks/MovieActivity.java
+++ app/src/main/java/com/psd/flicks/MovieActivity.java
@@ -3,10 +3,12 @@ package com.psd.flicks;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.psd.flicks.models.Movie;
+import com.psd.flicks.models.MovieAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,11 +22,19 @@ public class MovieActivity extends AppCompatActivity {
     String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     ArrayList<Movie> movies;
+    MovieAdapter movieAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        //widgets
+        lvItems = (ListView) findViewById(R.id.lvMovies);
+        movies = new ArrayList<>(); //make sure to initialize before placing in adapter to prevent crashing
+        movieAdapter = new MovieAdapter(this, movies);
+        lvItems.setAdapter(movieAdapter);
 
         //initialize http client
         AsyncHttpClient client = new AsyncHttpClient();
@@ -35,7 +45,8 @@ public class MovieActivity extends AppCompatActivity {
                 JSONArray movieResults;
                 try {
                     movieResults = response.getJSONArray("results");
-                    movies = Movie.fromJSONArray(movieResults);
+                    movies.addAll(Movie.fromJSONArray(movieResults));
+                    movieAdapter.notifyDataSetChanged();
                     Log.d("DEBUG", movies.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
